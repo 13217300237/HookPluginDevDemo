8c849f542cbcb79db93ae4d5ab4bd9963d85dfca
zhouhang <18598925736@163.com>
2019-03-09 17:42:34 +0800

    优化代码

--- plugin/src/main/java/study/hank/com/plugin/BaseActivity.java
+++ plugin/src/main/java/study/hank/com/plugin/BaseActivity.java
@@ -17,11 +17,11 @@ public class BaseActivity extends AppCompatActivity {
         return super.getResources();
     }
 
-    @Override
-    public AssetManager getAssets() {
-        if (getApplication() != null && getApplication().getAssets() != null) {
-            return getApplication().getAssets();
-        }
-        return super.getAssets();
-    }
+//    @Override
+//    public AssetManager getAssets() {//这个没必要重写，因为上面重写了getResource就是读取了宿主的Resources，而AssetManager是单例的，一旦发生融合，上面getResources拿到的assetManager一定是融合之后的
+//        if (getApplication() != null && getApplication().getAssets() != null) {
+//            return getApplication().getAssets();
+//        }
+//        return super.getAssets();
+//    }
 }
